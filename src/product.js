cb89c40873e253445597d23f6a2f312933e17f91
anthizy <ebojeleanthony01@gmail.com>
2023-10-24 14:43:24 +0100

    update

--- src/product.js
+++ src/product.js
@@ -1,6 +1,6 @@
 const product = {
-    name: "Iphone 15 Pro Max",
-    price: "$1,500",
+    name: "Iphone 14 Pro Max",
+    price: "$1,600",
     description: "Apple IPhone 15 Pro Max 256gb - Nano Sim - Blue Titanium.",
     imageurl:"./images/prod1.jpg"
   };
